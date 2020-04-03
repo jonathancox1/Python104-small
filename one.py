d0223b4c565bdd125518e72460d3b0104e52a2bc
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-03 17:41:21 -0400

    added file

---
+++ one.py
@@ -0,0 +1,16 @@
+#print number from 0 to 99
+# for i in range(100):
+#     print(i)
+
+
+
+#print the even number from 0 to 99
+# for i in range(100):
+#     if i % 2 == 0:
+#         print(i)
+
+
+#print the odd numbers from 0 to 99
+# for i in range (100):
+#     if i % 2 != 0:
+#         print(i)
